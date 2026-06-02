604073f88fc48dcb8a8ef659dc6bd57ecf2b1c35
fer3b_1884 <2006SKA@gmail.com>
2026-06-02 18:32:10 +0300

    test(reader): added test for ArrayFileReader class for reading data from file
    Signed-off-by: fer3b_1884 <2006SKA@gmail.com>

--- src/test/java/by/innowise/arraystask/reader/ArrayFileReaderTest.java
+++ src/test/java/by/innowise/arraystask/reader/ArrayFileReaderTest.java
@@ -1,22 +1,32 @@
 package by.innowise.arraystask.reader;
 
+import by.innowise.arraystask.exception.ArrayTaskException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayFileReaderTest {
+    private ArrayFileReader reader;
 
     @BeforeEach
     void setUp() {
+        reader = new ArrayFileReader();
+    }
+
+    @Test
+    void readFileData() throws ArrayTaskException {
+        String validPath = "src/main/resources/data/numbers.txt";
+        List<String> lines = reader.readFileData(validPath);
+
+        boolean actual = lines.isEmpty();
+        assertFalse(actual);
     }
 
     @AfterEach
     void tearDown() {
-    }
-
-    @Test
-    void readFileData() {
     }
 }
